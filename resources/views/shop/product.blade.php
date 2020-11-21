6ea756a0bee7058db44468fa5c273762f6be5ce7
Yurii <yuriy.zieit@gmail.com>
2020-11-21 18:06:55 +0200

    some

--- resources/views/shop/product.blade.php
+++ resources/views/shop/product.blade.php
@@ -9,52 +9,20 @@
                     @foreach(json_decode($product->photos) as $photo)
                     <div class="item__slide">
                         <a>
-<<<<<<< HEAD
-                            <img src="{{asset('img/product-1.png')}}" alt="" />
-                        </a>
-                    </div>
-                    <div class="item__slide">
-                        <a>
-                            <img src="{{asset('img/product-1.png')}}" alt="" />
-                        </a>
-                    </div>
-                    <div class="item__slide">
-                        <a>
-                            <img src="{{asset('img/product-1.png')}}" alt="" />
-                        </a>
-                    </div>
-=======
                             <img src="{{url('/storage/'.$photo)}}" alt="{{$photo}}" />
                         </a>
                     </div>
                     @endforeach
->>>>>>> bda7e2e3148e7c289000adbf9bd6abcb63db013d
                 </div>
 
                 <div class="item__thumbnails">
                     @foreach(json_decode($product->photos) as $photo)
                     <div class="item__slide">
                         <a>
-<<<<<<< HEAD
-                            <img src="{{asset('img/product-1.png')}}" alt="" />
-                        </a>
-                    </div>
-                    <div class="item__slide">
-                        <a>
-                            <img src="{{asset('img/product-1.png')}}" alt="" />
-                        </a>
-                    </div>
-                    <div class="item__slide">
-                        <a>
-                            <img src="{{asset('img/product-1.png')}}" alt="" />
-                        </a>
-                    </div>
-=======
                             <img src="{{url('/storage/'.$photo)}}" alt="{{$photo}}" />
                         </a>
                     </div>
                     @endforeach
->>>>>>> bda7e2e3148e7c289000adbf9bd6abcb63db013d
                 </div>
             </div>
 
